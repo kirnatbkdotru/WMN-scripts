a471a13a9ad2a615a7879afe9abcb95859034eab
kirnatbkdotru <kirn@bk.ru>
2012-01-09 23:29:13 +0400

    mail.ru auth URL change

--- mail.ru.js
+++ mail.ru.js
@@ -9,12 +9,12 @@ var hostString="mail.ru";
 function init() {
   var ar=this.user.split("@");
   if(!ar[1])ar[1]="mail.ru";
-	this.dataURL="http://win.mail.ru/cgi-bin/auth";
-	this.loginData=["http://win.mail.ru/cgi-bin/auth",,"Password","Login="+encodeURIComponent(ar[0])+"&Domain="+encodeURIComponent(ar[1])];
-	this.mailURL="http://win.mail.ru/cgi-bin/msglist?folder=0&4347803";
+	this.dataURL="http://e.mail.ru/cgi-bin/msglist";
+	this.loginData=["http://e.mail.ru/cgi-bin/auth",,"Password","Login="+encodeURIComponent(ar[0])+"&Domain="+encodeURIComponent(ar[1])];
+	this.mailURL="http://e.mail.ru/cgi-bin/msglist";
 }
 function getCount(aData) {
-	var fnd = aData.match(/g_mail_events.+?(\d)\D+?<\/i>/);
+	var fnd = aData.match(/g_mail_events.+?(\d)<\/i>/);
 	return fnd?(fnd[1]?fnd[1]:0):-1;
 }
 
