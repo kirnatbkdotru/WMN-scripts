68c2b38b13f350d127ff0c297a45b04413b4cb5b
kirnatbkdotru <kirn@bk.ru>
2012-01-10 00:07:19 +0400

    mail.ru Antonim fix

--- mail.ru.js
+++ mail.ru.js
@@ -1,23 +1,23 @@
 /**********************************************************
-@MAIL.RU
-  @author: butekx // modified  by Kiryanov Nikolay kirn@bk.ru
+    @MAIL.RU
+    @author: butekx // modified  by Kiryanov Nikolay kirn@bk.ru
 **********************************************************/
-var name="mail.RU";
-var ver="2011-07-09";
+var name="Mail.RU";
+var ver="2011-11-30";
 var hostString="mail.ru";
 
 function init() {
-  var ar=this.user.split("@");
-  if(!ar[1])ar[1]="mail.ru";
-	this.dataURL="http://e.mail.ru/cgi-bin/msglist";
-	this.loginData=["http://e.mail.ru/cgi-bin/auth",,"Password","Login="+encodeURIComponent(ar[0])+"&Domain="+encodeURIComponent(ar[1])];
-	this.mailURL="http://e.mail.ru/cgi-bin/msglist";
+    var ar=this.user.split("@");
+    if(!ar[1])ar[1]="mail.ru";
+    this.dataURL="http://e.mail.ru/cgi-bin/msglist";
+    this.loginData=["https://e.mail.ru/cgi-bin/auth",,"Password","Login="+encodeURIComponent(ar[0])+"&Domain="+encodeURIComponent(ar[1])];
+    this.mailURL="http://e.mail.ru/cgi-bin/msglist";
 }
 function getCount(aData) {
-	var fnd = aData.match(/g_mail_events.+?(\d)<\/i>/);
-	return fnd?(fnd[1]?fnd[1]:0):-1;
+    var fnd = aData.match(/g_mail_events.+?(\d+)<\/i>/);
+    return fnd?(fnd[1]?fnd[1]:0):-1;
 }
 
 function getIconURL(){
-  return "http://img.imgsmail.ru/r/ru/favicon.ico?1";
+    return "http://img.imgsmail.ru/r/ru/favicon.ico?1";
 }
