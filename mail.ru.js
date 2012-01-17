49c138dfab01310e8cc56d5903a601b0683d74b3
kirnatbkdotru <kirn@bk.ru>
2012-01-17 22:38:31 +0400

    changed to more reliable way to query mail coount

--- mail.ru.js
+++ mail.ru.js
@@ -9,15 +9,16 @@ var hostString="mail.ru";
 function init() {
     var ar=this.user.split("@");
     if(!ar[1])ar[1]="mail.ru";
-    this.dataURL="http://e.mail.ru/cgi-bin/msglist";
+    this.dataURL="http://swa.mail.ru/cgi-bin/counters";
     this.loginData=["https://e.mail.ru/cgi-bin/auth",,"Password","Login="+encodeURIComponent(ar[0])+"&Domain="+encodeURIComponent(ar[1])];
     this.mailURL="http://e.mail.ru/cgi-bin/msglist";
 }
 function getCount(aData) {
-    var fnd = aData.match(/g_mail_events.+?(\d+)<\/i>/);
-    return fnd?(fnd[1]?fnd[1]:0):-1;
+	var jdata = JSON.parse(aData);
+	if (jdata.status == "ok") return jdata.data.mail_cnt;
+	return -1;
 }
 
 function getIconURL(){
-    return "http://img.imgsmail.ru/r/ru/favicon.ico?1";
+	return "http://img.imgsmail.ru/r/ru/favicon.ico?1";
 }
