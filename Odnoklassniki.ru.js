54d5c0d22e5855ad721198ea9c17fb2dc3318a2e
kirnatbkdotru <kirn@bk.ru>
2012-01-22 23:24:46 +0400

    rewritten to use new JSON counter request

--- Odnoklassniki.ru.js
+++ Odnoklassniki.ru.js
@@ -6,30 +6,41 @@ var name="Odnoklassniki.RU";
 var ver="2010-11-26";
 
 function init() {
-	
- 	this.dataURL="http://www.odnoklassniki.ru";
 	this.loginData=["http://www.odnoklassniki.ru/dk?cmd=AnonymLogin","st.email","st.password","st.posted=set"];
 	this.mailURL="http://www.odnoklassniki.ru/dk?st.cmd=userMessageIncoming";
 }
 function getCount(aData) {
-	fnd = aData.match(/<div id="data_ToolbarIconConversation" class="hookData">(.+?)<\/div>/);
-	
-	if (!fnd) return - 1;
-	
-	var list = eval('(' + fnd[1] + ')').conversations;	
-	
-	if (list == null || list.length < 1)
-	{
-		return -1;
-	}
-	
-	unreadItemCount = 0;
-	
-	for (var i = 0; i < list.length; i++)
-	{
-		var item = list[i];
-			unreadItemCount += item.unreadItemCount;
-	}  
-	return unreadItemCount;
+	return JSON.parse(aData.substr(83, aData.length - 2 - 83)).data.ok_cnt; 
 }
 
+//return true if we specify the stage
+function process(aHttpChannel, aData) {
+  switch(this.stage){
+  case ST_LOGIN_RES:  
+  var regx = "(http://ok.mail.ru.+)'";
+	var fnd = aData.match(regx);
+	if (fnd) {
+		this.getHtml(fnd[1]);
+		return false;
+	}
+		
+  case ST_LOGIN_RES + 1:  
+	var self=this;
+	  chrome.cookies.getAll({domain:".mail.ru", name:"odklmapi"},
+		function(cookies){
+		  self.cookies = cookies;
+		})
+		
+	if (this.cookies){
+		var odklmapi = this.cookies[0].value;
+		this.stage = ST_DATA;
+		this.dataURL = "http://odnoklassniki.ru/mapi/getCounters?query=%7B%22jsonPrefix%22%3A%22PortalHeadlineOkJSONPCallback%22%2C%22odklmapi%22%3A%22"
+		+ encodeURIComponent(odklmapi) + "%22%7D";
+    }
+    break;
+  }
+  return this.baseProcess(aHttpChannel, aData);
+} 
+
+http://odnoklassniki.ru/mapi/getCounters?query=%7B%22jsonPrefix%22%3A%22PortalHeadlineOkJSONPCallback%22%2C%22odklmapi%22%3A%22%24%245OhA6nz3nLlT6%2FX3FruyrzOGzANX3gH6Rc%22%7D
+http://odnoklassniki.ru/mapi/getCounters?query=7B%22jsonPrefix%22%3A%22PortalHeadlineOkJSONPCallback%22%2C%22odklmapi%22%3A%22%24%245OhA6nz3nLlT6%2FX3FruyrzOGzANX3gH6Rc%22%7D
