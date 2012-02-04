99e99761e4e535a77be4849f998f5b4d811ddafe
kirnatbkdotru <kirn@bk.ru>
2012-02-05 00:53:23 +0400

    before changing to different data url

--- Odnoklassniki.ru.js
+++ Odnoklassniki.ru.js
@@ -3,26 +3,26 @@
   @author: Kiryanov Nikolay kirn@bk.ru
 **********************************************************/
 var name="Odnoklassniki.RU";
-var ver="2010-11-26";
+var ver="2012-01-23";
 
 function init() {
 	this.loginData=["http://www.odnoklassniki.ru/dk?cmd=AnonymLogin","st.email","st.password","st.posted=set"];
-	this.mailURL="http://www.odnoklassniki.ru/dk?st.cmd=userMessageIncoming";
+	this.mailURL="http://www.odnoklassniki.ru";
 }
 function getCount(aData) {
 	return JSON.parse(aData.substr(83, aData.length - 2 - 83)).data.ok_cnt; 
 }
 
-//return true if we specify the stage
 function process(aHttpChannel, aData) {
   switch(this.stage){
-  case ST_LOGIN_RES:  
-  var regx = "(http://ok.mail.ru.+)'";
-	var fnd = aData.match(regx);
+  case ST_LOGIN_RES:
+	re = "(http://ok.mail.ru.+)'";
+	var fnd = aData.match(re);
 	if (fnd) {
 		this.getHtml(fnd[1]);
 		return false;
 	}
+	break
 		
   case ST_LOGIN_RES + 1:  
 	var self=this;
@@ -38,9 +38,12 @@ function process(aHttpChannel, aData) {
 		+ encodeURIComponent(odklmapi) + "%22%7D";
     }
     break;
+	
+  case ST_DATA:
+	this.getHtml(this.dataURL, "" , {"Referer": "http://mail.ru/"});
+	return false;
+	break
+	
   }
   return this.baseProcess(aHttpChannel, aData);
-} 
-
-http://odnoklassniki.ru/mapi/getCounters?query=%7B%22jsonPrefix%22%3A%22PortalHeadlineOkJSONPCallback%22%2C%22odklmapi%22%3A%22%24%245OhA6nz3nLlT6%2FX3FruyrzOGzANX3gH6Rc%22%7D
-http://odnoklassniki.ru/mapi/getCounters?query=7B%22jsonPrefix%22%3A%22PortalHeadlineOkJSONPCallback%22%2C%22odklmapi%22%3A%22%24%245OhA6nz3nLlT6%2FX3FruyrzOGzANX3gH6Rc%22%7D
+}
